ece674ba8450928fde7de60738c0175338f5ccef
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-10 19:08:33 -0300

    Tab Navigator

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+
+
+function Following() {
+  return (
+     <> 
+      </>
+  );
+};
+
+export default Following;
