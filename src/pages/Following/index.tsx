cac27eb4bed65583b47d386d90e9bbdcff344e9d
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-10 20:42:25 -0300

    mockando dados estaticos

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -4,7 +4,60 @@ import Header from '../../components/Header';
 
 import { Wrapper, Container, Main } from './styles';
 
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
+
 function Following() {
+  const Following: React.FC = () => {
+  const { data, indices } = React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <Heading>Following</Heading>,
+      },
+
+      {
+        key: 'FOLLOWED_CATEGORIES',
+        render: () => <Title>Followed Categories</Title>,
+        isTitle: true,
+      },
+      { key: 'C1', render: () => <CategoryList /> },
+
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => <Title>Live Channels</Title>,
+        isTitle: true,
+      },
+      { key: 'C2', render: () => <StreamList /> },
+
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C3', render: () => <View /> },
+
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <Title>Offline Channels</Title>,
+        isTitle: true,
+      },
+      { key: 'C4', render: () => <ChannelList /> },
+    ];
+
+    const indices: number[] = [];
+
+    items.forEach((item, index) => item.isTitle && indices.push(index));
+
+    return {
+      data: items,
+      indices,
+    };
+  }, []);
+
   return (
     <Wrapper>
       <Container>
